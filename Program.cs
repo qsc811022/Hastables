ede71660e0fca902f6ee45e423c45b8c94c63945
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-11-14 17:19:09 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hastables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+}
